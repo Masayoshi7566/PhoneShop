0cfd72ab96dd9ab4e66f9f75a11c1ccf4b7d9d9b
Orathai <orathai.u@g-able.com>
2019-12-13 00:33:31 +0700

    Setup PhoneShop Project

---
+++ PhoneShop/Services/ImageCacheService.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCacheService.swift
+//  PhoneShop
+//
+//  Created by GABLET04690 on 12/12/2562 BE.
+//  Copyright © 2562 Orathai Udomluk. All rights reserved.
+//
+
+import Foundation
---
+++ PhoneShop/Views/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhoneShop
+//
+//  Created by GABLET04690 on 11/12/2562 BE.
+//  Copyright © 2562 Orathai Udomluk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
